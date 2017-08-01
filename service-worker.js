099386ced9442209c7f0ff03c45b5c038ccd6ab0
Samyaka <samyaka@Samyakas-iMac.local>
2017-08-01 16:22:43 +0530

    commit

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+var cacheName = 'pwa-commits-v3';
+
+var filesToCache = [
+    './app/app.html'
+];
+
+// Install Service Worker
+self.addEventListener('install', function(event) {
+
+    console.log('Service Worker: Installing....');
+
+    event.waitUntil(
+
+        // Open the Cache
+        caches.open(cacheName).then(function(cache) {
+            console.log('Service Worker: Caching App Shell at the moment......');
+
+            // Add Files to the Cache
+            return cache.addAll(filesToCache);
+            console.log('files added to cache');
+        })
+    );
+});
+
+
+// Fired when the Service Worker starts up
+self.addEventListener('activate', function(event) {
+
+    console.log('Service Worker: Activating....');
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(cacheNames.map(function(key) {
+                if( key !== cacheName) {
+                    console.log('Service Worker: Removing Old Cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+
+self.addEventListener('fetch', function(event) {
+
+    console.log('Service Worker: Fetch', event.request.url);
+
+    console.log("Url", event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
