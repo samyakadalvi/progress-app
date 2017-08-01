673aa8abaf2970efa7fe75fa8637b159f6797f4a
Samyaka <samyaka@Samyakas-iMac.local>
2017-08-01 18:35:39 +0530

    as

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,31 @@ var cacheName = 'pwa-commits-v3';
 var filesToCache = [
     'index.html'
 ];
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+  'messagingSenderId': '329708754726'
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
 
 // Install Service Worker
 self.addEventListener('install', function(event) {
